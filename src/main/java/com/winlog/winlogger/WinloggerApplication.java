4594e82e0eb1eb82931e48cfd01770460f3d350e
ValentineWa <waweruval@gmail.com>
2025-08-20 12:07:14 +0300

    Initial skeleton setup

---
+++ src/main/java/com/winlog/winlogger/WinloggerApplication.java
@@ -0,0 +1,13 @@
+package com.winlog.winlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinloggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winlog/winlogger/WinloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winlog.winlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
